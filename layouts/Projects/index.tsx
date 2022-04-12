f4b5041293d614492df159a00fa1155aad830c1b
keleris32 <charles.ejiegbu@gmail.com>
2022-04-13 00:04:25 +0100

    feat: added Projects section

---
+++ layouts/Projects/index.tsx
@@ -0,0 +1,11 @@
+import styles from './Projects.module.css';
+
+const Projects = () => {
+  return (
+    <section className={styles.container}>
+      <div></div>
+    </section>
+  );
+};
+
+export default Projects;
--- layouts/index.ts
+++ layouts/index.ts
@@ -1,2 +1,3 @@
 export { default as HeroSection } from './HeroSection';
 export { default as Wrapper } from './Wrapper';
+export { default as Projects } from './Projects';
